434e62a3dfa33fa16d75e2a28bc451b20ee7b097
sngsp <ssleerok@gmail.com>
2024-03-07 19:27:39 +0900

    delete

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,23 +5,8 @@ var greeting = "Hello, playground"
 
 
 print("JJJJJuuuunnnn  sdfasfsfasf")
-
-print("제발 고쳐져라 1111111111")
-print("되면 이거때문임")
-
-print("저녁 뭐먹지?")
-
-print("겹치면 어떻게 되는가?")
-
-
-print("불닭먹고싶당")
-
-
 print("되면 밥 좀 제발요 좀")
 
-<<<<<<< HEAD
 print("될때가 되었다.")
-=======
 print("될때가 되었는데요.")
 
->>>>>>> seop
