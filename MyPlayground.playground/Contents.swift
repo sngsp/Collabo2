a48a241d578ddc40255dfe3164b28dc625ea46ea
sngsp <ssleerok@gmail.com>
2024-03-07 19:49:48 +0900

    please

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,28 +1,32 @@
 import UIKit
 
+print("겹치면 어떻게 되는가?")
+print("불닭먹고싶당")
+print("되면 밥 좀 제발요 좀")
+print("될때가 되었는데요.")
+print("저녁 뭐먹지?")
+print("겹치면 어떻게 되는가?")
+print("불닭먹고싶당")
+print("되면 밥 좀 제발요 좀")
+print("될때가 되었는데요.")
 var greeting = "Hello, playground"
 print("될때가 되었다.")
-print("되면 이거때문")print("될때가 되었다.")
 print("되면 이거때문")
-print("저녁 뭐먹지?")print("될때가 되었다.")
+print("될때가 되었다.")
+print("되면 이거때문")
+print("저녁 뭐먹지?")
+print("될때가 되었다.")
 print("되면 이거때문")
 print("저녁 뭐먹지?")
 
 
 
 
-var greeting2 = 2
+var greeting = 23
+
+for i in 0...3 {
+    print("제발요")
+}
 
 
-
-
-print("겹치면 어떻게 되는가?")
-print("불닭먹고싶당")
-print("되면 밥 좀 제발요 좀")
-print("될때가 되었는데요.")
-print("저녁 뭐먹지?")
-print("겹치면 어떻게 되는가?")
-print("불닭먹고싶당")
-print("되면 밥 좀 제발요 좀")
-print("될때가 되었는데요.")
 
